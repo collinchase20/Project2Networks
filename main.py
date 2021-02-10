fafc242e9b17f92d50b8f074fb1de71aa2665a96
Collin Chase <collinc20@gmail.com>
2021-02-09 20:49:41 -0500

    NLJL
    LJ

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def main():
     ftps_interpreter.do_connect("networks-teaching-ftp.ccs.neu.edu")
 
     if not len(sys.argv) > 1:
-        print("Please pass an argument to the program." + "\n" + parser.print_help())
+        print("Please pass an argument to the program." + "\n")
+        #print(parser.print_help())
     elif (args.ls is not None):
         ftps_interpreter.do_list(args.urlLS)
     elif (args.mkdir is not None):
