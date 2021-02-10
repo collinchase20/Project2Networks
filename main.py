f8d798199454fa2c5a202b673a47e60fd36e2984
Collin Chase <collinc20@gmail.com>
2021-02-09 21:24:24 -0500

    hlkjl
    jlj

--- main.py
+++ main.py
@@ -17,22 +17,30 @@ def main():
 
     args = parser.parse_args()
 
-    ftps_interpreter = FtpInterpreter()
-
-    ftps_interpreter.do_connect("networks-teaching-ftp.ccs.neu.edu")
-
     if (not args.ls and not args.mkdir and not args.rm and not args.rmdir):
         print("Please provide one valid FTP operation (ls, mkdir, rm, rmdir)")
     elif (args.ls is not None):
+        ftps_interpreter = initialConnect()
         ftps_interpreter.do_list(args.url)
     elif (args.mkdir is not None):
+        ftps_interpreter = initialConnect()
         ftps_interpreter.do_mkdir(args.url)
     elif (args.rm is not None):
+        ftps_interpreter = initialConnect()
         ftps_interpreter.do_rm(args.url)
     elif (args.rmdir is not None):
+        ftps_interpreter = initialConnect()
         ftps_interpreter.do_rmdir(args.url)
     else:
         print("Should Not Come Here")
+
+
+def initialConnect():
+    ftps_interpreter = FtpInterpreter()
+    ftps_interpreter.do_connect("networks-teaching-ftp.ccs.neu.edu")
+    ftps_interpreter.do_login()
+    return ftps_interpreter
+
 
 
 
