05d0232410292c844a81281696b038e3c2395a7a
Collin Chase <collinc20@gmail.com>
2021-02-11 20:28:42 -0500

    lhjbhjvb
    lhlhkl

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
     args = parser.parse_args()
     interpreter = FtpInterpreter()
 
+    if (args.command == None or args.url == None):
+        return Exception ("You need to provide at least a command and FTP URL")
+
     #Get the username and password from ftp url
     initialString = args.url.split("@")[0]
     password = initialString.split(":")[2]
@@ -21,28 +24,41 @@ def main():
     username = process.split("//")[1]
 
     #Get the url
-    initalString2 = args.url.split("@")[1]
+    initialString2 = args.url.split("@")[1]
+    directoryString = ""
+    urlSplit = initialString2.split("/")
+    for i in range(len(urlSplit)):
+        if i == 0:
+            continue
+        else:
+            directoryString += "/"
+            directoryString += urlSplit[i]
+
+
+
+    print("The directory string is:" + directoryString)
+
 
 
     if (args.command == "ls"):
         print("Starting list process")
         initialConnect(interpreter, username, password)
-        interpreter.do_list("Test")
+        interpreter.do_list(directoryString)
         interpreter.do_disconnect()
     elif (args.command == "mkdir"):
         print("Starting make directory process")
         initialConnect(interpreter, username, password)
-        interpreter.do_mkdir("/MyMadeDirectory")
+        interpreter.do_mkdir(directoryString)
         interpreter.do_disconnect()
     elif (args.command == "rm"):
         print("Starting rm process")
         initialConnect(interpreter, username, password)
-        #interpreter.do_rm(args.url)
+        interpreter.do_rm(directoryString)
         interpreter.do_disconnect()
     elif (args.command == "rmdir"):
         print("Starting remove directory process")
         initialConnect(interpreter, username, password)
-        interpreter.do_rmdir("/Test")
+        interpreter.do_rmdir(directoryString)
         interpreter.do_disconnect()
     else:
         print("Please provide a valid command (ls, mkdir, rmdir, rm")
